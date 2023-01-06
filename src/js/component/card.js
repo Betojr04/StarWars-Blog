62d4fb6114312f372169e24a3417ba1f4a854b30
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-01-06 02:22:46 +0000

    starting adding a card on the home page and added a title, description, and place holder image

---
+++ src/js/component/card.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export const card = () => {
+    return (
+        <div className="card-container">
+            <div className="card-title">
+                <h4>Luke Skywalker</h4>
+            </div>
+            <div className="card-image">
+                <img src="/workspace/react-hello-webapp/src/img/rigo-baby.jpg"/>
+            </div>
+            <div className="card-description">
+
+            </div>
+        </div>
+    );
+};
+export default card;
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,9 +2,21 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
+
 export const Home = () => (
-	<div className="">
+	<div>
 		<h1>Characters</h1>
+		<div className="card-container">
+			<div className="card-title">
+				<h4>Luke Skywalker</h4>
+			</div>
+			<div className="card-image">
+				<img src="https://plus.unsplash.com/premium_photo-1661503325300-1d6289b4f53e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aHRtbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+			</div>
+			<div className="card-description">
+				<p>This is practice for the actual project</p>
+			</div>
+		</div>
 	</div>
 );
 
