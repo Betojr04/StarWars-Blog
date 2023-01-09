0d74951cbdb214cf1e22dbe482e8626124dde894
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-01-09 23:14:24 +0000

    added favorites drop down button on the navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,18 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">Star Wars</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Favorites</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light mb-3">
+      <Link to="/">
+        <span className="navbar-brand mb-0 h1">Star Wars</span>
+      </Link>
+      <div className="dropdown">
+        <button
+          class="btn btn-secondary dropdown-toggle"
+          type="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Favorites
+        </button>
+        <ul className="dropdown-menu">
+          <li>
+            <a className="dropdown-item" href="#">
+              Action
+            </a>
+          </li>
+          <li>
+            <a className="dropdown-item" href="#">
+              Another action
+            </a>
+          </li>
+          <li>
+            <a className="dropdown-item" href="#">
+              Something else here
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
